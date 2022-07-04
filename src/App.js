7cc6f3d9f34f43b86479c98b71f1b576919123b3
zihooy139@gmail.com <zihooy139@gmail.com>
2022-07-04 14:56:33 +0900

    create Dockerfile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <body>
+      <div className="container">
+        <div className="mt-5 text-center fs-1">
+          This is demo project of Jihu Yang
+        </div>
+      </div>
+
+    </body>
+      <footer class="text-white-50">
+        <div className="text-center">
+        <p>Demo project by <a href="https://github.com/zihooy" class="text-white">@Jihu Yang</a>.</p>
+        </div>
+     
+    </footer>
     </div>
   );
 }
